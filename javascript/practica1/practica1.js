4ec55382f8d2e4c618c1ed2c891b4f6f584dedd8
tonifp1729 <tonifp1729@gmail.com>
2024-01-25 13:45:03 +0100

    Momento de confusión. Solo subo las reglas de juego.

--- javascript/practica1/practica1.js
+++ javascript/practica1/practica1.js
@@ -1,1 +1,11 @@
-//Aquí irá la programación del juego en un principio
+/**
+  * REGLAS DE JUEGO - Duels OF Fates
+  * 
+  * Shii-Cho. Pierde en cualquiera de los encuentros, incluido contra sí mismo.
+  * Makashi. Vence a soresu, niman. 
+  * Soresu. Vence contra ataru, niman.
+  * Ataru. Vence a makasi, niman.
+  * Shien/Djem So. Vence contra ataru, soresu.
+  * Niman. Vence a sien, pierde los demás.
+  * Juyo/Vaapad. Pierde contra ataru, gana todos los demás.
+  **/
