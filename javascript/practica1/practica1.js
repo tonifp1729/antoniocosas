08e4dd542579a2b4fa93ee2093d7ffaf3fd8bd2d
tonifp1729 <tonifp1729@gmail.com>
2024-01-30 13:22:03 +0100

    Duels of Fates - Cambio de vistas e inicio y fin de la música al cambiar de vista.

--- javascript/practica1/practica1.js
+++ javascript/practica1/practica1.js
@@ -9,3 +9,47 @@
   * Niman. Vence a sien, pierde los demás.
   * Juyo/Vaapad. Pierde contra ataru, gana todos los demás.
   **/
+
+
+//Inicializamos las vistas, elementos con los que vamos a trabajar:
+let pantallaInicial = document.getElementById("pantallaInicial")
+let pantallaJugador = document.getElementById("pantallaJugador")
+let pantallaFinal = document.getElementById("pantallaFinal")
+
+//Inicializamos los distintos elementos que vamos a trabajar en cada vista.
+//pantallaInicial
+let botonIniciar = document.getElementById("botonIniciar")
+let musica = document.querySelector("audio")
+
+//pantallaJugador
+let contenedorJugada = document.getElementById("jugada")
+let botonTerminar = document.getElementById("botonTerminar")
+
+//pantallaFinal
+let botonVolver = document.getElementById("botonVolver")
+
+
+//VISTA - pantallaInicial
+botonIniciar.addEventListener('click', e => {
+  pantallaInicial.style.display = 'none'
+  pantallaJugador.style.display = 'flex'
+  musica.play()
+})
+
+
+//VISTA - pantallaJugador
+botonTerminar.addEventListener('click', e => {
+  pantallaJugador.style.display = 'none'
+  pantallaFinal.style.display = 'flex'
+})
+
+function finJuego() {
+  musica.pause();
+  musica.currentTime = 0;
+}
+
+//VISTA - pantallaFinal
+botonVolver.addEventListener('click', e => {
+  pantallaFinal.style.display = 'none'
+  pantallaInicial.style.display = 'flex'
+})
