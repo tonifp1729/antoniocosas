e246b22f7213cf90ac3071e8bae0e8665ab34385
tonifp1729 <tonifp1729@gmail.com>
2024-01-31 13:44:54 +0100

    Programación del reglamento completado. ¡¡A por el DOMgit add .

--- javascript/practica1/practica1.js
+++ javascript/practica1/practica1.js
@@ -12,21 +12,23 @@
 
 
 //Inicializamos las vistas, elementos con los que vamos a trabajar:
-let pantallaInicial = document.getElementById("pantallaInicial")
-let pantallaJugador = document.getElementById("pantallaJugador")
-let pantallaFinal = document.getElementById("pantallaFinal")
+const pantallaInicial = document.getElementById("pantallaInicial")
+const pantallaJugador = document.getElementById("pantallaJugador")
+const pantallaFinal = document.getElementById("pantallaFinal")
 
 //Inicializamos los distintos elementos que vamos a trabajar en cada vista.
 //pantallaInicial
-let botonIniciar = document.getElementById("botonIniciar")
-let musica = document.querySelector("audio")
+const botonIniciar = document.getElementById("botonIniciar")
+const musica = document.querySelector("audio")
 
 //pantallaJugador
-let contenedorJugada = document.getElementById("jugadas")
-let botonTerminar = document.getElementById("botonTerminar")
+const contenedorJugada = document.getElementById("jugadas")
+const botonTerminar = document.getElementById("botonTerminar")
+let puntosJugador
+let puntosRival
 
 //pantallaFinal
-let botonVolver = document.getElementById("botonVolver")
+const botonVolver = document.getElementById("botonVolver")
 
 
 //VISTA - pantallaInicial
@@ -34,6 +36,8 @@ botonIniciar.addEventListener('click', e => {
   pantallaInicial.style.display = 'none'
   pantallaJugador.style.display = 'flex'
   musica.play()
+  puntosJugador = 0
+  puntosRival = 0
 })
 
 
@@ -41,19 +45,120 @@ botonIniciar.addEventListener('click', e => {
 botonTerminar.addEventListener('click', e => {
   pantallaJugador.style.display = 'none'
   pantallaFinal.style.display = 'flex'
+  musica.pause();
+  musica.currentTime = 0;
 })
 
 //Hemos capturado el evento click dentro del contenedor
 contenedorJugada.addEventListener('click', e => {
-  //Si se clica sobre un elemento de tipo botón dentro del contenedor qeu hemos capturado, el div jugada, guardamos el texto.
+  //Si se clica sobre un elemento de tipo botón dentro del contenedor que hemos capturado, el div jugada, guardamos el texto.
   if(e.target.tagName === 'button') {
-    let jugadaSeleccionada = e.target.innerText;
+    jugadaSeleccionada = e.target.id;
   }
+
+  hacerJugada(jugadaSeleccionada)
 })
 
-function finJuego() {
-  musica.pause();
-  musica.currentTime = 0;
+function aleatorio() {
+  const min = 1
+  const max = 7
+  let numero = Math.floor(Math.random() * (max - min + 1) + minimo);
+  return numero;
+}
+
+function jugadaRival() {
+  let numero = aleatorio()
+
+  switch (numero) {
+    case 1:
+      return "forma1"
+    case 2:
+      return "forma2"
+    case 3:
+      return "forma3"
+    case 4:
+      return "forma4"
+    case 5:
+      return "forma5"
+    case 6:
+      return "forma6"
+    case 7:
+      return "forma7"
+  }
+}
+
+function hacerJugada(jugadaSeleccionada) {
+  let rival = jugadaRival()
+  let resultado
+
+  switch (jugadaSeleccionada) {
+    case "forma1": //HECHO
+      if(rival==="forma1") {
+        resultado=0
+      }else {
+        resultado=1
+      }
+    case "forma2": //HECHO
+      if(rival==="forma3" || rival==="forma6" || rival==="forma1") {
+        resultado=2
+      }else if(rival==="forma4" || rival==="forma7") {
+        resultado=1
+      }else {
+        resultado=0
+      }
+    case "forma3": //HECHO
+      if(rival==="forma4" || rival==="forma6" || rival==="forma1") {
+        resultado=2
+      }else if(rival==="forma2" || rival==="forma7") {
+        resultado=1
+      }else {
+        resultado=0
+      }
+    case "forma4": //HECHO
+      if(rival==="forma2" || rival==="forma6" || rival==="forma7" || rival==="forma1") {
+        resultado=2
+      }else if(rival==="forma3" || rival==="forma5") {
+        resultado=1
+      }else {
+        resultado=0
+      }
+    case "forma5": //HECHO
+      if(rival==="forma3" || rival==="forma4" || rival==="forma1") {
+        resultado=2
+      }else if(rival==="forma2" || rival==="forma7") {
+        resultado=1
+      }else {
+        resultado=0
+      }
+    case "forma6": //HECHO
+      if(rival==="forma5" || rival==="forma1") {
+        resultado=2
+      }else if(rival==="forma2" || rival==="forma3" || rival==="forma4" || rival==="forma7") {
+        resultado=1
+      }else {
+        resultado=0
+      }
+    case "forma7": //HECHO
+      if(rival==="forma4") {
+        resultado=1
+      }else if(rival==="forma7") {
+        resultado=0
+      }else {
+        resultado=2
+      }
+  }
+
+  if(resultado===0) {
+    //Empate = +1 a ambos
+    puntosJugador++
+    puntosRival++
+  }else if(resultado===1) {
+    //Derrota = +1 a rival
+    puntosRival++
+  }else if(resultado===2) {
+    //Victoria = +1 a jugador
+    puntosJugador++
+  }
 }
 
 //VISTA - pantallaFinal
